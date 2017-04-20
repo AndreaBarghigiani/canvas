266697de5a7dbc6be8965028ed874db4bd07ca51
anna-wro <wro.online@gmail.com>
2017-04-20 21:28:23 +0200

    Project files

---
+++ app/js/main.js
@@ -0,0 +1,68 @@
+var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+var mouseMovedOffCanvas = false;
+var $restOfPage = $("body");
+$(".controls").on("click", "li", function () {
+  $(this).siblings().removeClass("selected");
+  if ($(this).attr('id') != 'revealColorSelect') {
+    $(this).addClass("selected");
+    color = $(this).css("background-color");
+  }
+});
+$("#revealColorSelect").click(function () {
+  $("#colorSelect").toggle();
+});
+
+function changeColor() {
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
+}
+
+$("#addNewColor").click(function () {
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $($newColor).insertBefore("#revealColorSelect");
+  $newColor.click();
+});
+
+$("input[type=range]").on('input', function(){
+  changeColor();
+});
+$canvas.mousedown(function (e) {
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function (e) {
+  if (mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function () {
+  mouseDown = false;
+}).mouseleave(function () {
+  if (mouseDown === true) {
+    mouseMovedOffCanvas = true;
+  }
+  else {
+    mouseMovedOffCanvas = false;
+  }
+  mouseDown = false;
+}).mouseenter(function (e) {
+  lastEvent = e;
+  if (mouseMovedOffCanvas === true) {
+    mouseDown = true;
+    $canvas.mousedown(e);
+  }
+});
+$restOfPage.mouseup(function () {
+  mouseMovedOffCanvas = false;
+  mouseDown = false;
+});
