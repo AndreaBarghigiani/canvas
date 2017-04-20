228d142205da81bd16940fed72a4c8997bae91fa
anna-wro <wro.online@gmail.com>
2017-04-20 21:28:54 +0200

    :sparkles: Update chosen color immediately

--- old code/js/app.js
+++ old code/js/app.js
@@ -1,64 +1,75 @@
-var color = $(".selected").css("background-color");
-var $canvas = $("canvas");
-var context = $canvas[0].getContext("2d");
-var lastEvent;
-var mouseDown = false;
-var mouseMovedOffCanvas = false;
-var $restOfPage = $("body");
-$(".controls").on("click", "li", function () {
-    $(this).siblings().removeClass("selected");
-    if ($(this).attr('id') != 'revealColorSelect') {
-        $(this).addClass("selected");
-        color = $(this).css("background-color");
-    }
-});
-$("#revealColorSelect").click(function () {
-    $("#colorSelect").toggle();
+var color = $('.selected').css('background-color')
+var $canvas = $('canvas')
+var context = $canvas[0].getContext('2d')
+var lastEvent
+var mouseDown = false
+var mouseMovedOffCanvas = false
+var $restOfPage = $('body')
+$('.controls').on('click', 'li', function () {
+  $(this).siblings().removeClass('selected')
+  if ($(this).attr('id') != 'revealColorSelect') {
+    $(this).addClass('selected')
+    color = $(this).css('background-color')
+  }
+})
+$('#revealColorSelect').click(function () {
+  $('#colorSelect').toggle()
+})
+
+function changeColor () {
+  var r = $('#red').val()
+  var g = $('#green').val()
+  var b = $('#blue').val()
+  $('#newColor').css('background-color', 'rgb(' + r + ',' + g + ', ' + b + ')')
+}
+$('input[type=range]').onmousemove(function () {
+  changeColor();
+})
+
+$('[data-slider]').on('mousemove', function(){
+  var r = $('#red').val()
+  var g = $('#green').val()
+  var b = $('#blue').val()
+  $('#newColor').css('background-color', 'rgb(' + r + ',' + g + ', ' + b + ')')
 });
 
-function changeColor() {
-    var r = $("#red").val();
-    var g = $("#green").val();
-    var b = $("#blue").val();
-    $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
-}
-$("input[type=range]").change(changeColor);
-$("#addNewColor").click(function () {
-    var $newColor = $("<li></li>");
-    $newColor.css("background-color", $("#newColor").css("background-color"));
-    $($newColor).insertBefore("#revealColorSelect");
-    $newColor.click();
-});
+
+$('#addNewColor').click(function () {
+  var $newColor = $('<li></li>')
+  $newColor.css('background-color', $('#newColor').css('background-color'))
+  $($newColor).insertBefore('#revealColorSelect')
+  $newColor.click()
+})
 $canvas.mousedown(function (e) {
-    lastEvent = e;
-    mouseDown = true;
+  lastEvent = e
+  mouseDown = true
 }).mousemove(function (e) {
-    if (mouseDown) {
-        context.beginPath();
-        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-        context.lineTo(e.offsetX, e.offsetY);
-        context.strokeStyle = color;
-        context.stroke();
-        lastEvent = e;
-    }
+  if (mouseDown) {
+    context.beginPath()
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY)
+    context.lineTo(e.offsetX, e.offsetY)
+    context.strokeStyle = color
+    context.stroke()
+    lastEvent = e
+  }
 }).mouseup(function () {
-    mouseDown = false;
+  mouseDown = false
 }).mouseleave(function () {
-    if (mouseDown === true) {
-        mouseMovedOffCanvas = true;
-    }
-    else {
-        mouseMovedOffCanvas = false;
-    }
-    mouseDown = false;
+  if (mouseDown === true) {
+    mouseMovedOffCanvas = true
+  }
+  else {
+    mouseMovedOffCanvas = false
+  }
+  mouseDown = false
 }).mouseenter(function (e) {
-    lastEvent = e;
-    if (mouseMovedOffCanvas === true) {
-        mouseDown = true;
-        $canvas.mousedown(e);
-    }
-});
+  lastEvent = e
+  if (mouseMovedOffCanvas === true) {
+    mouseDown = true
+    $canvas.mousedown(e)
+  }
+})
 $restOfPage.mouseup(function () {
-    mouseMovedOffCanvas = false;
-    mouseDown = false;
-});
+  mouseMovedOffCanvas = false
+  mouseDown = false
+})
