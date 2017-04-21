9cc142bee4ebb4b8d944a7559b0ba1cbec74f6cc
anna-wro <wro.online@gmail.com>
2017-04-21 17:59:19 +0200

    :wrench: JavaScript - adding colors

--- app/js/main.js
+++ app/js/main.js
@@ -1,91 +1,105 @@
 var canvas = document.getElementsByClassName('drawing-area')[0];
-var context = canvas.getContext("2d");
+var context = canvas.getContext('2d');
 var lastEvent;
 var mouseDown = false;
 var mouseMovedOffCanvas = false;
-var inputs = document.querySelectorAll("input[type=range]");
-var restOfPage = document.getElementsByTagName("body");
+var inputs = document.querySelectorAll('input[type=range]');
+var restOfPage = document.getElementsByTagName('body');
 var selected = document.getElementsByClassName('selected')[0];
-var color = window.getComputedStyle(selected,null).getPropertyValue('background-color');
-var red = document.getElementById("red");
-var green = document.getElementById("green");
-var blue = document.getElementById("blue");
-var newColor = document.getElementsByClassName("color-new")[0];
+var color = window.getComputedStyle(selected, null).getPropertyValue('background-color');
+var red = document.getElementById('red');
+var green = document.getElementById('green');
+var blue = document.getElementById('blue');
+var newColor = document.getElementsByClassName('color-new')[0];
 var buttonReveal = document.getElementById('revealColorSelect');
+var buttonAdd = document.getElementById('addNewColor');
 var selectionDiv = document.getElementById('colorSelect');
+var colorElements = document.getElementsByClassName('color-element');
+var availableColors = document.getElementsByClassName('colors-list')[0];
 
+// Selecting color
 
-$(".controls").on("click", "li", function () {
-  $(this).siblings().removeClass("selected");
-  if ($(this).attr('id') !== 'revealColorSelect') {
-    $(this).addClass("color-element color selected");
-    color = $(this).css("background-color");
-  }
-});
+  for (var i = 0; i < colorElements.length; i++) {
+    colorElements[i].addEventListener('click', function (e) {
+      var current = this;
+      for (var i = 0; i < colorElements.length; i++) {
+        if (current !== colorElements[i]) {
+          colorElements[i].classList.remove('selected')
+        } else if (current.classList.contains('selected') === true) {
+          current.classList.remove('selected')
+        } else {
+          if (current.id !== 'revealColorSelect') {
+            current.classList.add('selected');
+          }
+        }
+      }
+      e.preventDefault()
+    })
+}
 
 // Toggle visibility of adding panel
 
 buttonReveal.addEventListener('click', function () {
-  selectionDiv.style.display =  selectionDiv.style.display === 'block'? 'none' : 'block';
+  selectionDiv.style.display = selectionDiv.style.display === 'block' ? 'none' : 'block'
 })
 
 // Change color - sliders
 
-function changeColor() {
-  var r = red.value;
-  var g = green.value;
-  var b = blue.value;
-  newColor.style.backgroundColor = "rgb(" + r + "," + g + ", " + b + ")";
+function changeColor () {
+  var r = red.value
+  var g = green.value
+  var b = blue.value
+  newColor.style.backgroundColor = 'rgb(' + r + ',' + g + ', ' + b + ')'
 }
 
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('input', changeColor, false);
+for (var j = 0; j < inputs.length; j++) {
+  inputs[j].addEventListener('input', changeColor, false)
 }
 
 // Add new color
 
-$("#addNewColor").click(function () {
-  var $newColor = $("<li></li>");
-  $newColor.css("background-color", $(".color-new").css("background-color"));
-  $($newColor).insertBefore("#revealColorSelect");
-  $newColor.click();
-});
+buttonAdd.addEventListener('click', function () {
+  var addColor = document.createElement('li')
+  addColor.className = 'color-element color'
+  addColor.style.backgroundColor = newColor.style.backgroundColor
+  availableColors.insertBefore(addColor, buttonReveal)
+})
 
 // Drawing functions
 
-canvas.onmousedown = function(e){
-  lastEvent = e;
-  mouseDown = true;
+canvas.onmousedown = function (e) {
+  lastEvent = e
+  mouseDown = true
 }
 
 canvas.onmousemove = function (e) {
   if (mouseDown) {
-    context.beginPath();
-    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-    context.lineTo(e.offsetX, e.offsetY);
-    context.strokeStyle = color;
-    context.stroke();
-    lastEvent = e;
+    context.beginPath()
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY)
+    context.lineTo(e.offsetX, e.offsetY)
+    context.strokeStyle = color
+    context.stroke()
+    lastEvent = e
   }
 }
 
-canvas.onmouseup = function(e){
-  mouseDown = false;
+canvas.onmouseup = function () {
+  mouseDown = false
 }
 
-canvas.onmouseout = function(e){
-  mouseMovedOffCanvas = mouseDown === true;
-  mouseDown = false;
+canvas.onmouseout = function () {
+  mouseMovedOffCanvas = mouseDown === true
+  mouseDown = false
 }
 
 canvas.onmouseover = function (e) {
-  lastEvent = e;
+  lastEvent = e
   if (mouseMovedOffCanvas === true) {
-    mouseDown = true;
+    mouseDown = true
   }
 }
 
-restOfPage.onmouseup = function(e){
-  mouseMovedOffCanvas = false;
-  mouseDown = false;
+restOfPage.onmouseup = function () {
+  mouseMovedOffCanvas = false
+  mouseDown = false
 }
